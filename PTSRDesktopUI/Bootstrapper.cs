f4974f11f81efcd58bf46c5baee3bded6a174490
rei.berberi <rei.berberi@BIEWS2149>
2020-01-10 09:55:30 +0100

    LoginForm created

---
+++ PTSRDesktopUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using PTSRDesktopUI.ViewModels;
+using System.Windows;
+
+namespace PTSRDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+    }
+}
